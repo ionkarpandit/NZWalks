31113b9c150aa41c655279b42d425d46f5e01f73
Onkar Sharma <royalvatsa@gmail.com>
2026-01-03 22:46:48 +0530

    Created Auth Controller and Register Action Method

---
+++ NZWalks.API/Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.DTO;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        // POST: api/auth/Register
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto registerRequestDto)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = registerRequestDto.UserName,
+                Email = registerRequestDto.UserName
+            };
+
+            var identityResult = await userManager.CreateAsync(identityUser, registerRequestDto.Password);
+
+            if (identityResult.Succeeded)
+            {
+                // Add Roles to User
+                if (registerRequestDto.Roles != null && registerRequestDto.Roles.Any())
+                {
+                    identityResult = await userManager.AddToRolesAsync(identityUser, registerRequestDto.Roles);
+
+                    if (identityResult.Succeeded)
+                    {
+                        return Ok("User has been registered! Please login.");
+                    }
+                }
+            }
+
+            return BadRequest("User registration failed. Please try again.");
+        }
+    }
+}
