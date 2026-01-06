4be01227bfbe2c01e9143a828ba0657526c0a57c
Onkar Sharma <royalvatsa@gmail.com>
2026-01-06 22:46:17 +0530

    Serving Static File

--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -3,6 +3,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using NZWalks.API.Data;
@@ -139,6 +140,14 @@ app.UseHttpsRedirection();
 
 app.UseAuthentication(); // JWT Authentication Configuration: Add Authentication Middleware
 app.UseAuthorization();
+
+// To serve static files from wwwroot folder, like images
+app.UseStaticFiles(new StaticFileOptions
+{
+    // Serve files from "Images" folder
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Images")),
+    RequestPath = "/Images"
+});
 
 app.MapControllers();
 
